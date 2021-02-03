5ec61a32dd8ecb8018ffe4f8ae2f45327e396f3c
vnstr <vnstr.main@gmail.com>
2021-02-03 19:50:32 +0300

    start philo_one, create main

---
+++ philo_one/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdrive <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/03 18:22:09 by gdrive            #+#    #+#             */
+/*   Updated: 2021/02/03 18:22:10 by gdrive           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**	args:
+**		number_of_philos
+**		time_to_die (lifetime after eating)
+**		time_to_eat (philo will spend for eating)
+**		time_to_sleep (philo will spend for sleep)
+**		number_of_times_each_philo_must_eat
+**
+**		P.s. Time in miliseconds.
+*/
+
+int		main(int argc, char **argv)
+{
+	return (0);
+}
