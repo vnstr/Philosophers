574a7de45747fb075073236f587ae6b01c69dc61
vnstr <vnstr.main@gmail.com>
2021-02-06 22:41:56 +0300

    added more tests to compile_and_run.sh

--- philo_one/compile_and_run.sh
+++ philo_one/compile_and_run.sh
@@ -3,6 +3,19 @@
 	#make && ./philo_one 4 1000 25 50 10
 
 #nobody die
-	make && ./philo_one 5 800 200 200 10
-#someone should die
-	#make && ./philo_one 4 1 25 50 10
+#	make && ./philo_one 5 800 200 200
+
+#each philos must eat 7 times
+#	make && ./philo_one 5 800 200 7
+
+#nobody die
+	make && ./philo_one 4 410 200 200
+
+#philos should die
+#	make && ./philo_one 4 310 200 100
+
+#check rigth die-time (correct in 1500ms)
+#	make && ./philo_one 200 1500 500 2000
+
+#check rigth die-time (correct in 1500ms)
+#	make && ./philo_one 4 410 200 200
